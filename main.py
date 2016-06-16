f2ebd1a96127c02b19086b1884de8c5a06943dcd
Feihong Hsu <feihong.hsu@gmail.com>
2016-06-16 11:03:35 -0500

    Show a random greeting

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-$('#content').text('Hello World!')
+import random
 
-def on_click():
-    $('#content').text('Goodbye World!')
-$('button').on('click', on_click)
+greetings = [
+    'Hello World',
+    'Hola Mundo',
+    'ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ ਦੁਨਿਆ',
+    'こんにちは世界',
+    '你好世界',
+    'Përshendetje Botë',
+    'مرحبا بالعالم',
+    'Բարեւ, աշխարհ',
+    'হ্যালো দুনিয়া',
+    'Saluton mondo',
+    'გამარჯობა მსოფლიო',
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+$('#content').text(get_random_greeting())
+
+
+$('button').on('click', def():
+    $('#content').text(get_random_greeting())
+)
